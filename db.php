520d96ce2fc5b0dfede4dbc55efaf8553d655328
rkmjld2 <rkmjld2@gmail.com>
2026-08-01 13:13:07 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-$user="ax6KHc1BNkyuaor.root",
-$password="ax6KHc1BNkyuaor.root",
-$dbname="studentdb",
+$host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$user="ax6KHc1BNkyuaor.root";
+$password="ax6KHc1BNkyuaor.root";
+$dbname="studentdb";
 
 $conn=mysqli_connect($host,$user,$password,$dbname);
 
