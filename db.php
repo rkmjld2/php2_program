13959b44d1e7d41fb44c553125fbb5e3b777bc45
rkmjld2 <rkmjld2@gmail.com>
2026-08-01 18:16:41 +0530

    Update db.php

--- db.php
+++ db.php
@@ -6,36 +6,12 @@ $user = "ax6KHc1BNkyuaor.root";
 $password = "EP8isIWoEOQk7DSr";
 $dbname = "studentdb";
 
-/* Initialize MySQLi */
-$conn = mysqli_init();
 
-/* Enable SSL/TLS */
-mysqli_ssl_set(
-    $conn,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-);
 
-/* Connect to TiDB Cloud using SSL */
-mysqli_real_connect(
-    $conn,
-    $host,
-    $user,
-    $password,
-    $dbname,
-    $port,
-    NULL,
-    MYSQLI_CLIENT_SSL
-);
+$conn=mysqli_connect($host,$user,$password,$dbname);
 
-/* Check connection */
-if (mysqli_connect_errno()) {
-    die("Connection Failed: " . mysqli_connect_error());
+if(!$conn)
+{
+die("Connection Failed");
 }
-
-echo "TiDB Cloud connection successful!";
-
 ?>
