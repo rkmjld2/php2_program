6d60fdb76b48a61c1dce80a6bd638d371505673c
rkmjld2 <rkmjld2@gmail.com>
2026-08-01 12:49:36 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,16 @@
+<?php
+
+include "db.php";
+
+$name=$_POST['name'];
+
+$course=$_POST['course'];
+
+$sql="INSERT INTO students(name,course)
+VALUES('$name','$course')";
+
+mysqli_query($conn,$sql);
+
+header("Location:index.php");
+
+?>
